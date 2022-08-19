bc853e49ebaf7c23921ef870a12da9b2cb1c1059
Daniel Natan Schmitz <danielnatanschmitz@gmail.com>
2022-08-19 20:50:36 -0300

    Correção exercicio

--- Aula1808/src/refactoring/basic/VerificaPoliedro.java
+++ Aula1808/src/refactoring/basic/VerificaPoliedro.java
@@ -3,41 +3,72 @@ package refactoring.basic;
 import java.util.Scanner;
 
 public class VerificaPoliedro {
+
+	static boolean isTetraedro(int numeroArestas, int numeroVertices){
+		if((numeroVertices==4) && (numeroArestas==6)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	static boolean isHexaedro(int numeroArestas, int numeroVertices){
+		if((numeroVertices==8) && (numeroArestas==12)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	static boolean isOctaedro(int numeroArestas, int numeroVertices){
+		if((numeroVertices==6) && (numeroArestas==12)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	static boolean isDodecaedro(int numeroArestas, int numeroVertices){
+		if((numeroVertices==20) && (numeroArestas==30)) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	static boolean isIcosaedro(int numeroArestas, int numeroVertices){
+		if((numeroVertices==12) && (numeroArestas==30)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	static int retornaNumeroFaces(int numeroArestas, int numeroVertices){
+		return 2+numeroArestas-numeroVertices;
+	}
 	
 	static void determinarPoliedro(int numeroArestas, int numeroVertices){
-		boolean	encontrouResultado;
 		int numeroFaces;
-		
-		encontrouResultado=false;
-		numeroFaces=2+numeroArestas-numeroVertices;
 
-		if((numeroVertices==4) && (numeroArestas==6)){
+		numeroFaces = retornaNumeroFaces(numeroArestas,numeroVertices);
+
+		if(isTetraedro(numeroArestas,numeroVertices)){
 			System.out.println("Tetraedro:"+ numeroFaces+" faces");
 			System.out.println("Forma face: Triângulo");
-			encontrouResultado=true;
-		}else {
-			if((numeroVertices==8) && (numeroArestas==12)){
-				System.out.println("Hexaedro:"+numeroFaces+" faces");
-				System.out.println("Forma face: Quadrado");
-				encontrouResultado=true;
-			}
-			if((numeroVertices==6) && (numeroArestas==12)){
-				System.out.println("Octaedro:"+numeroFaces+" faces");
-				System.out.println("Forma face: Triângulo");
-				encontrouResultado=true;
-			}
-			if((numeroVertices==20) && (numeroArestas==30)) {
-				System.out.println("Dodecaedro:"+numeroFaces+" faces");
-				System.out.println("Forma face: Pentágono");
-				encontrouResultado=true;
-			}
-			if((numeroVertices==12) && (numeroArestas==30)){
-				System.out.println("Icosaedro:"+numeroFaces+" faces");
-				System.out.println("Forma face: Triângulo");
-				encontrouResultado=true;
-			}
-		}
-		if(encontrouResultado==false){
+		}else if(isHexaedro(numeroArestas,numeroVertices)){
+			System.out.println("Hexaedro:"+numeroFaces+" faces");
+			System.out.println("Forma face: Quadrado");
+		}else if(isOctaedro(numeroArestas,numeroVertices)){
+			System.out.println("Octaedro:"+numeroFaces+" faces");
+			System.out.println("Forma face: Triângulo");
+		}else if(isDodecaedro(numeroArestas,numeroVertices)) {
+			System.out.println("Dodecaedro:" + numeroFaces + " faces");
+			System.out.println("Forma face: Pentágono");
+		}else if(isIcosaedro(numeroArestas,numeroVertices)){
+			System.out.println("Icosaedro:"+numeroFaces+" faces");
+			System.out.println("Forma face: Triângulo");
+		} else {
 			System.out.println("Os dados não indicam nenhum poliedro regular.");
 		}
 	}
